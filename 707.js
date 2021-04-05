730b9f342660eb8c01b414d2748111ac18397f22
Daniel-Eprog <39196999+Daniel-Eprog@users.noreply.github.com>
2021-04-05 16:01:20 -0400

    update file case

--- 707.js
+++ 707.js
@@ -27,67 +27,67 @@ buttonAnimation(event.key);
 function makeSound(key){
   switch (key) {
     case "q":
-      var kick = new Audio("sounds/707kick.WAV");
+      var kick = new Audio("sounds/707kick.wav");
       kick.play();
     break;
 
     case "w":
-      var snare = new Audio("sounds/707snare.WAV");
+      var snare = new Audio("sounds/707snare.wav");
       snare.play();
     break;
 
     case "e":
-      var lowtom = new Audio("sounds/707lowtom.WAV");
+      var lowtom = new Audio("sounds/707lowtom.wav");
       lowtom.play();
     break;
 
     case "r":
-      var midtom = new Audio("sounds/707midtom.WAV");
+      var midtom = new Audio("sounds/707midtom.wav");
       midtom.play();
     break;
 
     case "t":
-      var hitom = new Audio("sounds/707hitom.WAV");
+      var hitom = new Audio("sounds/707hitom.wav");
       hitom.play();
     break;
 
     case "y":
-      var rim = new Audio("sounds/707rimshot.WAV");
+      var rim = new Audio("sounds/707rimshot.wav");
       rim.play();
     break;
 
     case "u":
-      var cowbell = new Audio("sounds/707cowbell.WAV");
+      var cowbell = new Audio("sounds/707cowbell.wav");
       cowbell.play();
     break;
 
     case "i":
-      var clap = new Audio("sounds/707clap.WAV");
+      var clap = new Audio("sounds/707clap.wav");
       clap.play();
     break;
 
     case "o":
-      var tambourine = new Audio("sounds/707tambourine.WAV");
+      var tambourine = new Audio("sounds/707tambourine.wav");
       tambourine.play();
     break;
 
     case "p":
-      var hhc = new Audio("sounds/707HHC.WAV");
+      var hhc = new Audio("sounds/707HHC.wav");
       hhc.play();
     break;
 
     case "a":
-      var hho = new Audio("sounds/707HHO.WAV");
+      var hho = new Audio("sounds/707HHO.wav");
       hho.play();
     break;
 
     case "s":
-      var crash = new Audio("sounds/707crash.WAV");
+      var crash = new Audio("sounds/707crash.wav");
       crash.play();
     break;
 
     case "d":
-      var ride = new Audio("sounds/707ride.WAV");
+      var ride = new Audio("sounds/707ride.wav");
       ride.play();
     break;
 
